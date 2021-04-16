11d79e212741b759461077b6e549db2269cebf4e
Chorn Thorn <chornthorn.kh@gmail.com>
2021-04-17 01:09:59 +0700

    Enable cors in applicaiton

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.enableCors(); // Enable cors for frontend access
+  app.setGlobalPrefix('api'); // Set prefix "api" to global after www.domain.com
+  app.useGlobalPipes(new ValidationPipe()); // Set global validationPipe to any class or entity
+  await app.listen(3000); // Application listen on port : 3000
 }
 bootstrap();
