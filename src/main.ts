060741178610e8afb195eda14ca900258a9ff36e
Chorn Thorn <chornthorn.kh@gmail.com>
2021-04-22 04:53:51 +0700

    add transform interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './utilities/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable cors for frontend access
   app.setGlobalPrefix('api'); // Set prefix "api" to global after www.domain.com
   app.useGlobalPipes(new ValidationPipe()); // Set global validationPipe to any class or entity
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3000); // Application listen on port : 3000
 }
 bootstrap();
